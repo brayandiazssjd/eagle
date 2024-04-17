194c38f11c5c459d3f3c6912049906d29a71e385
Brayan Diaz <brayandiazssjd@gmail.com>
2024-04-16 22:48:46 -0500

    Added full functionality for Queue.h

--- include/Queue.h
+++ include/Queue.h
@@ -10,16 +10,35 @@ class Queue {
 		int size;
 
 	public:
+		Queue() {
+			head = new Node<T>();
+			tail = new Node<T>();
+			head->next = tail;
+			head->value = 0;
+			tail->next = tail;
+			tail->value = 0;
+	}
+
 		bool isEmpty() {
 			return size == 0;
 		}
 
 		void enqueue (T e) {
-
+			tail->next = new Node<T>();
+			tail->value = e;
+			tail = tail->next;
+			tail->next = tail;
+			tail->value = 0;
+			size++;
 		}
 
-		T denqueue () {
-
+		T denqueue () {	
+			Node<T> *temp = head->next;
+			T e = temp->value;
+			head->next = head->next->next;
+			delete temp;
+			size--;
+			return e;
 		}
 };
 
