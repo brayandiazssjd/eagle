744a489466aed6c933f58fdaaa5fcf341b64b30c
Brayan Diaz <brayandiazssjd@gmail.com>
2024-05-10 20:43:04 -0500

    New remove method in 'List.h' and new 'STYLE.txt' with some style convention for the source code.

--- include/List.h
+++ include/List.h
@@ -5,110 +5,102 @@
 #include <cstddef>
 #include <stdexcept>
 
-template <typename T>
-class List {
+template <typename T> class List {
   Node<T> *head, *tail;
   int size;
 
 public:
   List() {
     head = new Node<T>();
-		tail = new Node<T>();
-		head->next = tail;
-		head->value = 0;
-		tail->next = tail;
-		tail->value = 0;
+    tail = new Node<T>();
+    head->next = tail;
     size = 0;
   }
 
-	~List() {
-		Node<T> *aux;
-		for (int i = 0; i < size+2; i++) {
-			aux = head->next;
-			delete head;
-			head = aux;
-		}
-		delete aux;
-		delete head;
-	}
-
-  bool validIndex(int index) {
-  	return -1 < index && index < size;
+  ~List() {
+    Node<T> *aux;
+    for (int i = 0; i < size + 2; i++) {
+      aux = head->next;
+      delete head;
+      head = aux;
+    }
+    delete aux;
+    delete head;
   }
+
+  bool validIndex(int index) { return -1 < index && index < size; }
 
   void add(T e) {
-    Node<T> *aux = head;
-    for (int i = 0; i < size; ++i) {
-    	aux = aux->next;
-    }
- 		Node<T> *temp = new Node<T>();
-		temp->value = e;
-		temp->next = tail;
-		aux->next = temp;
-
- 		delete aux;    
+    tail->value = e;
+    tail->next = new Node<T>();
+    tail = tail->next;
     size++;
-    
   }
 
-	void add(T e, int index) {
-		if(!validIndex(index))
-			throw std::out_of_range("Index out of bounds");
-		Node<T> *aux = head;
-		for (int i = 0; i < size; i++) {
-			aux = aux->next;
-		}
-		Node<T> *temp = new Node<T>();
-		temp->value = e;
-		temp->next = aux->next;
-		aux->next = temp;
-		delete aux;
-		delete temp;
-		size++;
-	}
+  void add(T e, int index) {
+    if (!validIndex(index))
+      throw std::out_of_range("Index out of bounds");
+    Node<T> *aux = head;
+    for (int i = 0; i < size; i++) {
+      aux = aux->next;
+    }
+    Node<T> *temp = new Node<T>();
+    temp->value = e;
+    temp->next = aux->next;
+    aux->next = temp;
+    size++;
+  }
 
   void addFirst(T e) {
     Node<T> *temp = new Node<T>();
     temp->next = head->next;
     temp->value = e;
     head->next = temp;
-    delete temp;
     size++;
   }
 
   void remove(int index) {
-  	if(!validIndex())
-  		throw std::out_of_range("Index out of bounds");
-  	Node<T> *aux = head;
-  	for (int i = 0; i < index; ++i) {
-  		aux = aux->next;
-  	}
-  	Node<T> temp = aux->next->next;
-		delete aux->next;
-		aux->next = temp;
-		delete temp;
-		delete aux;
-	}
+    if (!validIndex())
+      throw std::out_of_range("Index out of bounds");
+    Node<T> *aux = head;
+    for (int i = 0; i < index; ++i) {
+      aux = aux->next;
+    }
+    Node<T> temp = aux->next->next;
+    aux->next = temp;
+    delete temp;
+  }
+
+  void remove(T e) {
+    if (!validIndex())
+      throw std::out_of_range("Index out of bounds");
+    Node<T> *aux = head;
+    for (int i = 0; i < size; ++i) {
+      aux = aux->next;
+			if(aux->value == e) {
+
+			}
+    }
+    Node<T> temp = aux->next->next;
+    aux->next = temp;
+    delete temp;
+  }
 
   T get(int index) {
-  	if(!validIndex(index))
-  		throw std::out_of_range("Index out of bounds");
-		Node<T> *aux = head->next;
+    if (!validIndex(index))
+      throw std::out_of_range("Index out of bounds");
+    Node<T> *aux = head->next;
     for (int i = 0; i < index; i++) {
-    	aux = aux->next;
+      aux = aux->next;
     }
     T e = aux->value;
     delete aux;
     return e;
   }
 
-  bool isEmpty() { 
-		return size == 0;
-	}
+  bool isEmpty() { return size == 0; }
 
-	int length() {
-		return size;
-	}
+  int length() { return size; }
 };
 
 #endif
